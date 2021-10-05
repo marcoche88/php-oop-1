726afdc5caa2d085425e62822a26bd8b4d80b599
marcoche88 <marco.che@hotmail.it>
2021-10-05 15:21:48 +0200

    boilerplate

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- Oggi pomeriggio ripassate i primi concetti di classe e di proprietà e
+metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
+- è definita una classe ‘Movie’
+   => all'interno della classe sono dichiarate delle variabili d'istanza (proprietà)
+   => all'interno della classe è definito un costruttore
+   => all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà, se possibile cerchiamo di stampare senza ricorrere a var_dump -->
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie class</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
