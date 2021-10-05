d8962fd6cda5a41b7247f693632c8b214912234e
marcoche88 <marco.che@hotmail.it>
2021-10-05 15:51:27 +0200

    added object Movie

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
    => all'interno della classe è definito almeno un metodo
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà, se possibile cerchiamo di stampare senza ricorrere a var_dump -->
 
+<?php
+// importo il file con la classe Movie
+require_once __DIR__ . '/Movie.php';
+
+// istanziamento di due oggetti Movie
+$movie1 = new Movie("Il Signore degli Anelli - La Compagnia dell'Anello", "Fantasy", "2001", "USA", 4, "Peter Jackson");
+$movie2 = new Movie("Il Gladiatore", "Storico", "2000", "USA", 5, "Ridley Scott");
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 
