17ce2cdcd172551404e4a9f82f8b3796e47b9008
marcoche88 <marco.che@hotmail.it>
2021-10-05 15:38:54 +0200

    added class Movie

---
+++ Movie.php
@@ -0,0 +1,29 @@
+<?php
+
+class Movie
+{
+    // proprietà della classe
+    public $title;
+    public $director;
+    public $genre;
+    public $year;
+    public $country;
+    public $vote;
+
+    // costruttore dell'istanza
+    public function __construct($_title, $_genre, $_year, $_country, $_vote, $_director = "anonimo")
+    {
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->year = $_year;
+        $this->country = $_country;
+        $this->vote = $_vote;
+        $this->director = $_director;
+    }
+
+    //metodo della classe
+    public function getReview()
+    {
+        return "Il film " . $this->title . "ha ricevuto una valutazione di " . $this->vote . "/5";
+    }
+}
